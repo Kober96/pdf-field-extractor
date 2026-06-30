89cb886dbb0d5c42f48e606639a2896f66c6179d
Kober96 <daniel.kober@walter-tools.com>
2026-06-30 09:02:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ if uploaded_files:
         img = np.array(pages[0])
 
         # 👉 Koordinaten
-        roi = img[900:1300, 2000:2200]
+        roi = img[900:1300, 2200:2400]
 
         # 👉 in PIL umwandeln
         cropped = Image.fromarray(roi)
